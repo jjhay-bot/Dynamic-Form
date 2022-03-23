8214f4c0821f2d589a8f705127d992ee743cb743
Jhay Jhay Alcorcon <jhay.alcorcon@growsari.com>
2022-03-23 10:38:21 +0800

    fix deployment issue with fontawesome

--- src/components/ui/drawer.js
+++ src/components/ui/drawer.js
@@ -1,18 +1,19 @@
-import * as React from "react";
-import { Box, Stack, IconButton, Link } from "@mui/material";
-import SwipeableDrawer from "@mui/material/SwipeableDrawer";
-import Button from "@mui/material/Button";
-import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
-import ListItem from "@mui/material/ListItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import { useDispatch } from "react-redux";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import CompanyName from "./../header/companyName";
-import SupervisorAccountRoundedIcon from "@mui/icons-material/SupervisorAccountRounded";
-import CreateNewFolderRoundedIcon from "@mui/icons-material/CreateNewFolderRounded";
+import * as React from 'react';
+import { Box, Stack, IconButton, Link } from '@mui/material';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { useDispatch } from 'react-redux';
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faBars } from "@fortawesome/free-solid-svg-icons";
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import CompanyName from './../header/companyName';
+import SupervisorAccountRoundedIcon from '@mui/icons-material/SupervisorAccountRounded';
+import CreateNewFolderRoundedIcon from '@mui/icons-material/CreateNewFolderRounded';
 
 const setPageValue = false;
 
@@ -27,18 +28,22 @@ export default function SwipeableTemporaryDrawer() {
   // const { formModalShow } = useSelector(formModalState);
 
   const toggleDrawer = (anchor, open) => (event) => {
-    if (event && event.type === "keydown" && (event.key === "Tab" || event.key === "Shift")) {
+    if (
+      event &&
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
       return;
     }
-    setState({ ...state, ["left"]: open });
+    setState({ ...state, ['left']: open });
   };
 
   const list = () => (
     <Box
-      sx={{ width: 220, background: "#ffeef8", minHeight: "100vh" }}
+      sx={{ width: 220, background: '#ffeef8', minHeight: '100vh' }}
       role="presentation"
-      onClick={toggleDrawer("left", false)}
-      onKeyDown={toggleDrawer("left", false)}
+      onClick={toggleDrawer('left', false)}
+      onKeyDown={toggleDrawer('left', false)}
     >
       <List>
         <ListItem>
@@ -50,11 +55,11 @@ export default function SwipeableTemporaryDrawer() {
       <List>
         <ListItem>
           <ListItemIcon>
-            <Link to="/" style={{ zIndex: "10" }}>
-              <SupervisorAccountRoundedIcon />{" "}
+            <Link to="/" style={{ zIndex: '10' }}>
+              <SupervisorAccountRoundedIcon />{' '}
             </Link>
           </ListItemIcon>
-          <ListItemText primary={"Accounts"} />
+          <ListItemText primary={'Accounts'} />
         </ListItem>
 
         <ListItem>
@@ -63,7 +68,7 @@ export default function SwipeableTemporaryDrawer() {
               <CreateNewFolderRoundedIcon />
             </Link>
           </ListItemIcon>
-          <ListItemText primary={"Create Account"} />
+          <ListItemText primary={'Create Account'} />
         </ListItem>
       </List>
       <Divider />
@@ -72,21 +77,22 @@ export default function SwipeableTemporaryDrawer() {
 
   return (
     <div>
-      <React.Fragment key={"left"}>
-        <Button onClick={toggleDrawer("left", true)}>
+      <React.Fragment key={'left'}>
+        <Button onClick={toggleDrawer('left', true)}>
           <Stack direction="row" spacing={0}>
             <IconButton edge="start" aria-label="menu">
-              <FontAwesomeIcon icon={faBars} />
+              {/* <FontAwesomeIcon icon={faBars} /> */}
+              <MenuRoundedIcon />
             </IconButton>
           </Stack>
         </Button>
         <SwipeableDrawer
-          anchor={"left"}
-          open={state["left"]}
-          onClose={toggleDrawer("left", false)}
-          onOpen={toggleDrawer("left", true)}
+          anchor={'left'}
+          open={state['left']}
+          onClose={toggleDrawer('left', false)}
+          onOpen={toggleDrawer('left', true)}
         >
-          {list("left")}
+          {list('left')}
         </SwipeableDrawer>
       </React.Fragment>
     </div>
